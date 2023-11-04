efcac9b25e55645d1e9f9a5da626821c69901484
bkoubik88 <benjamin.koubik@gmx.de>
2023-11-04 18:50:08 +0100

    with bookmark site favicon german 89

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -26,7 +26,7 @@ export default function Navigation() {
           {isAuthenticated && !isLoading && (
             <UserButton afterSignOutUrl="/"></UserButton>
           )}
-          <SavedImages></SavedImages>
+          {isAuthenticated && !isLoading && <SavedImages></SavedImages>}
           <Toogle></Toogle>
         </div>
       </div>
